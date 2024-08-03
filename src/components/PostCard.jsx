c5df290d89e70aef13fe917165b07603f546184c
Aarogya Rijal <arijal@wpi.edu>
2024-08-03 01:37:05 -0400

    Fixed image selection priority

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import PostInfo from './PostInfo';
 import PostInteract from './PostInteract';
 import './PostCard.css';
@@ -6,6 +6,7 @@ import './PostCard.css';
 const PostCard = (props) => {
     const {source, url, title, published, images, videos, description, journalists, likes, shares, comments, saves, profilePic, setVideoRef, autoplay} = props;
     const videoRef = useRef(null);
+    const [imageSrc, setImageSrc] = useState('');
     let footer = source;
     if(journalists) {
         footer = source + " | " + journalists?.join(" & ");
@@ -24,6 +25,35 @@ const PostCard = (props) => {
     //     }
     // };
 
+    useEffect(() => {
+        const checkImage = (url) => {
+            return new Promise((resolve, reject) => {
+                const img = new Image();
+                img.src = url;
+                img.onload = () => resolve(url);
+                img.onerror = () => reject();
+            });
+        };
+
+        const loadImage = async () => {
+            if (!Array.isArray(images)) {
+                setImageSrc(images);
+                return;
+            }
+            for (const imageUrl of images.slice(1)) {
+                try {
+                    const validUrl = await checkImage(imageUrl);
+                    setImageSrc(validUrl);
+                    break;
+                } catch {
+                    // continue to next URL
+                }
+            }
+        };
+
+        loadImage();
+    }, [images]);
+
     return (
         <>
             <div className="video">
@@ -38,7 +68,7 @@ const PostCard = (props) => {
                 {/*    loop*/}
                 {/*    src={videos[0]}*/}
                 {/*></video>*/}
-                <img className="player" src={images[0]} />
+                <img className="player" src={imageSrc} />
             </div>
             <div className="bottom-controls">
                 <div className="footer-left">
